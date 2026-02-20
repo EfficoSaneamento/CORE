adea314d4406c9dd3be9070972c86f0f60e02142
EfficoSaneamento <planejamento@effico.com.br>
2026-02-20 19:55:10 +0000

    ajuste

--- script.js
+++ script.js
@@ -1,90 +1,40 @@
-const URL_API = 'https://script.google.com/macros/s/AKfycbynSax6Q0OuPIiHvkOtQ_BYIuQh15kZtLZSWPUbRKxPboKFNPi4lTO2IGBKhpxYXprK/exec';
+const URL_API = 'https://script.google.com/macros/s/AKfycbzmLvDrB8jTm-6pzBVBVNdAtwclN6ybaaUkwiai_HsrwgVkuaTy4l5KWKYY32CoK0kJ/exec';
 
-let abaAtiva = 'banco';
-
-/* ---------- NAVEGAÇÃO ---------- */
-function mudarAba(aba) {
-    abaAtiva = aba;
-    atualizarTitulo();
-    atualizarBotoes();
-    montarCabecalho();
-    carregarDados();
-}
-
-function atualizarTitulo() {
-    const titulos = {
-        banco: 'BANCO DE PREÇOS',
-        solicitacao: 'SOLICITAÇÃO DE COMPRAS',
-        avaliacao: 'AVALIAÇÃO DO FORNECEDOR'
-    };
-    document.getElementById('tituloPagina').innerText = titulos[abaAtiva];
-}
-
-function atualizarBotoes() {
-    ['banco', 'solicitacao', 'avaliacao'].forEach(a => {
-        document.getElementById(`btn-${a}`)
-            .classList.remove('bg-white/10');
-    });
-    document.getElementById(`btn-${abaAtiva}`)
-        .classList.add('bg-white/10');
-}
-
-/* ---------- TABELA ---------- */
-function montarCabecalho() {
-    const cabecalho = document.getElementById('cabecalhoTabela');
-
-    let colunas = [];
-
-    if (abaAtiva === 'banco') {
-        colunas = [
-            'ID_ORIGEM','DATA_CRIACAO','COD_REDUZIDO_CCUSTO',
-            'CODIGOPRD','PRODUTO','QUANTIDADEORC',
-            'UNIDADE','PRECO_UNITARIO','VALOR_TOTAL',
-            'FORNECEDOR','COMPRADOR'
-        ];
-    }
-
-    if (abaAtiva === 'solicitacao') {
-        colunas = [
-            'IDENTIFICADOR','DATA_SOLICITACAO','DATA_LIMITE',
-            'CENTRO_CUSTO','DESCRITIVO','QUANTIDADE','OBSERVACAO'
-        ];
-    }
-
-    if (abaAtiva === 'avaliacao') {
-        colunas = ['FORNECEDOR','NOTA','COMENTARIO'];
-    }
-
-    cabecalho.innerHTML = `
-        <tr>
-            ${colunas.map(c => `<th class="p-3 text-left">${c}</th>`).join('')}
-        </tr>
-    `;
-}
-
-/* ---------- DADOS ---------- */
 function carregarDados() {
     const corpo = document.getElementById('corpoTabela');
+
     corpo.innerHTML = `
         <tr>
-            <td colspan="20" class="p-4 text-center text-gray-400">
+            <td colspan="8" class="p-4 text-center text-gray-400">
                 Carregando...
             </td>
         </tr>
     `;
 
-    const sheet =
-        abaAtiva === 'solicitacao' ? 'DB_SOLICITACOES' :
-        abaAtiva === 'avaliacao' ? 'DB_AVALIACAO' :
-        'DB_ITENS';
-
-    fetch(`${URL_API}?aba=${sheet}`)
+    fetch(URL_API)
         .then(res => res.json())
-        .then(dados => renderizarTabela(dados))
+        .then(dados => {
+            corpo.innerHTML = '';
+
+            dados.forEach(l => {
+                corpo.innerHTML += `
+                    <tr class="border-t">
+                        <td class="p-3">${l.IDENTIFICADOR}</td>
+                        <td class="p-3">${l['Data da Solicitação']}</td>
+                        <td class="p-3">${l['Data Limite']}</td>
+                        <td class="p-3">${l['Centro de Custo']}</td>
+                        <td class="p-3">${l.Item}</td>
+                        <td class="p-3">${l.Observacao}</td>
+                        <td class="p-3">${l.Quantidade}</td>
+                        <td class="p-3">${l.Solicitante}</td>
+                    </tr>
+                `;
+            });
+        })
         .catch(() => {
             corpo.innerHTML = `
                 <tr>
-                    <td colspan="20" class="p-4 text-center text-red-500">
+                    <td colspan="8" class="p-4 text-center text-red-500">
                         Erro ao carregar dados
                     </td>
                 </tr>
@@ -92,54 +42,4 @@ function carregarDados() {
         });
 }
 
-function renderizarTabela(dados) {
-    const corpo = document.getElementById('corpoTabela');
-    corpo.innerHTML = '';
-
-    if (!dados || dados.length === 0) {
-        corpo.innerHTML = `
-            <tr>
-                <td colspan="20" class="p-4 text-center text-gray-400">
-                    Nenhum registro encontrado
-                </td>
-            </tr>
-        `;
-        return;
-    }
-
-    let colunas = [];
-
-    if (abaAtiva === 'banco') {
-        colunas = [
-            'ID_ORIGEM','DATA_CRIACAO','COD_REDUZIDO_CCUSTO',
-            'CODIGOPRD','PRODUTO','QUANTIDADEORC',
-            'UNIDADE','PRECO_UNITARIO','VALOR_TOTAL',
-            'FORNECEDOR','COMPRADOR'
-        ];
-    }
-
-    if (abaAtiva === 'solicitacao') {
-        colunas = [
-            'IDENTIFICADOR','DATA_SOLICITACAO','DATA_LIMITE',
-            'CENTRO_CUSTO','DESCRITIVO','QUANTIDADE','OBSERVACAO'
-        ];
-    }
-
-    if (abaAtiva === 'avaliacao') {
-        colunas = ['FORNECEDOR','NOTA','COMENTARIO'];
-    }
-
-    dados.forEach(linha => {
-        corpo.innerHTML += `
-            <tr class="border-t">
-                ${colunas.map(c =>
-                    `<td class="p-3">${linha[c] ?? ''}</td>`
-                ).join('')}
-            </tr>
-        `;
-    });
-}
-
-/* ---------- INICIAL ---------- */
-montarCabecalho();
 carregarDados();
